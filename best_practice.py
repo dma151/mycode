a1044de04f6598cdb09711a2dd597709d18ed60a
Daniel An <dma151@users.noreply.github.com>
2022-08-09 22:28:18 +0000

    finished up to lab 25

---
+++ best_practice.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""A multi-line comment to describe the script,
+    made with tripple quotes"""
+
+def main(): # all code should appear within a function
+    """all fcuntions have multiline comments to decribe them"""
+
+    my_string = "your code would go here" # vars use _ not camelCase
+    print(my_string) # print to standard-out
+
+# calling main() using this technique allows others to import your code
+if __name__ == "__main__":
+    main()
