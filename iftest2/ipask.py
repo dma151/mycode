f2d90a2c8d225beb5ff4e5a6c015404687a0cd56
Daniel An <dma151@users.noreply.github.com>
2022-08-11 16:37:54 +0000

    working on if, elif, else logic

---
+++ iftest2/ipask.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+ipchck = input("Apply an IP Address: ")
+
+if ipchck:
+    print("Looks like the IP address was set: " + ipchck)
+else:
+    print("You did not provide input")
