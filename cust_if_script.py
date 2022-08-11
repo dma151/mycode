5c0542ab5a56c9ac241bd8e5c41b16634cc0aaf5
Daniel An <dma151@users.noreply.github.com>
2022-08-11 19:57:33 +0000

    Learning to troubleshoot

---
+++ cust_if_script.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# Grader
+
+# Prompt a user for a score between 0 - 100
+score = float(input("What was your score? "))
+
+if score >= 90:
+    print("Not bad, A for Average")
+elif score >= 80:
+    print("What? B? B for Below Average")
+elif score >= 70:
+    print("C? What you doin? C for Can't believe you got a C.")
+elif score >= 60:
+    print("D? Are you D for DUMB?")
+else:
+    print("F? -Snatch your forehead-, No use for forehead when you got no brain")
+
+print("Thanks for choosing motivational Grader", "\n")
+
+# Movie Summary
+
+Movies = {
+    "harry potter" : {
+        "1":"Adaptation of the first of J.K. Rowling's popular children's novels about Harry Potter, a boy who learns on his eleventh birthday that he is the orphaned son of two powerful wizards and possesses unique magical powers of his own. He is summoned from his life as an unwanted child to become a student at Hogwarts, an English boarding school for wizards. There, he meets several friends who become his closest allies and help him discover the truth about his parents' mysterious deaths.",
+        "2":"The second instalment of boy wizard Harry Potter's adventures at Hogwarts School of Witchcraft and Wizardry, based on the novel by JK Rowling. A mysterious elf tells Harry to expect trouble during his second year at Hogwarts, but nothing can prepare him for trees that fight back, flying cars, spiders that talk and deadly warnings written in blood on the walls of the school.",
+        "3":"Harry Potter's (Daniel Radcliffe) third year at Hogwarts starts off badly when he learns deranged killer Sirius Black (Gary Oldman) has escaped from Azkaban prison and is bent on murdering the teenage wizard. While Hermione's (Emma Watson) cat torments Ron's (Rupert Grint) sickly rat, causing a rift among the trio, a swarm of nasty Dementors is sent to protect the school from Black. A mysterious new teacher helps Harry learn to defend himself, but what is his secret tie to Sirius Black?",
+        "4":"The fourth movie in the Harry Potter franchise sees Harry (Daniel Radcliffe) returning for his fourth year at Hogwarts School of Witchcraft and Wizardry, along with his friends, Ron (Rupert Grint) and Hermione (Emma Watson). There is an upcoming tournament between the three major schools of magic, with one participant selected from each school by the Goblet of Fire. When Harry's name is drawn, even though he is not eligible and is a fourth player, he must compete in the dangerous contest.",
+        "5":"Now in his fifth year at Hogwarts, Harry (Daniel Radcliffe) learns that many in the wizarding community do not know the truth of his encounter with Lord Voldemort. Cornelius Fudge, minister of Magic, appoints his toady, Dolores Umbridge, as Defense Against the Dark Arts teacher, for he fears that professor Dumbledore will take his job. But her teaching is deficient and her methods, cruel, so Harry prepares a group of students to defend the school against a rising tide of evil.",
+        "6":"As Death Eaters wreak havoc in both Muggle and Wizard worlds, Hogwarts is no longer a safe haven for students. Though Harry (Daniel Radcliffe) suspects there are new dangers lurking within the castle walls, Dumbledore is more intent than ever on preparing the young wizard for the final battle with Voldemort. Meanwhile, teenage hormones run rampant through Hogwarts, presenting a different sort of danger. Love may be in the air, but tragedy looms, and Hogwarts may never be the same again.",
+        "7":"Without the guidance and protection of their professors, Harry (Daniel Radcliffe), Ron (Rupert Grint) and Hermione (Emma Watson) begin a mission to destroy the Horcruxes, the sources of Voldemort's immortality. Though they must rely on one another more than ever, dark forces threaten to tear them apart. Voldemort's Death Eaters have seized control of the Ministry of Magic and Hogwarts, and they are searching for Harry -- even as he and his friends prepare for the ultimate showdown."
+        },
+    "lord of the rings": {
+        "1":""
+        }
+    }
+
+# Prompt user for a movie series
+movie = input("Which movie series synposis would you like to hear? ")
+number = input("Which movie in that series? (i.e 1, 2, 3) ")
+
+selected_series = Movies.get(movie.lower())
+selected_movie = selected_series.get(number)
+
+print(selected_movie)
