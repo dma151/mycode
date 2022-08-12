1be2b5c2246dd299a06475a5080d593f0aa03008
Daniel An <dma151@users.noreply.github.com>
2022-08-12 19:50:12 +0000

    Playing with python pip packages

---
+++ color/color3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import crayons
+
+def main():
+    print('Hello, my name is...')
+    print(f'{crayons.blue("Daniel")} {crayons.magenta("An")}')
+
+
+if __name__ == "__main__":
+    main()
