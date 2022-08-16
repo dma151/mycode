b2b6025a5e4cc3e065f56eb0eaedd51e6fd99326
Daniel An <dma151@users.noreply.github.com>
2022-08-16 14:51:39 +0000

    utilizing classes

--- listmethods/listmeth01.py
+++ listmethods/listmeth01.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+""" Author: Daniel
+    The purpose of this script it to learn and exercise list methods"""
+
 proto = ["ssh", "http", "https"]
 print(proto)
 print(proto[1])
---
+++ smallscript.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+""" Author: Daniel
+    The purpose of this script is to print a string"""
+
+MY_NOTE = "print this string"
+print(MY_NOTE)
